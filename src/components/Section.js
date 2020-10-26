2afdc827573980e28c300d70a7bc749d7bd53c4f
marloy <evgeniy.mirea@yandex.ru>
2020-10-26 20:19:15 +0300

    refactor: подготовить новую файловую структуру, добавить класс Section

---
+++ src/components/Section.js
@@ -0,0 +1,23 @@
+export default class Section {
+  constructor({ items, renderer }, containerSelector) {
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
+  }
+
+  clearContainer() {
+    this._container.innerHTML = '';
+  }
+
+  renderItems() {
+    this.clearContainer();
+
+    this._items.forEach(item => {
+      this._renderer(item);
+    });
+  }
+
+  addItem(element) {
+    this._container.append(element);
+  }
+}
