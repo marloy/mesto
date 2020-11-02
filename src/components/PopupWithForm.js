ee8808edea047dc1b6dbe66ffaf1230274edf99d
marloy <evgeniy.mirea@yandex.ru>
2020-11-02 22:21:10 +0300

    refactor: добавить класс PopupWithForm

---
+++ src/components/PopupWithForm.js
@@ -0,0 +1,28 @@
+import Popup from '../components/Popup.js';
+
+export default class PopupWithForm extends Popup {
+  constructor(popupSelector, handleSubmitForm) {
+    super(popupSelector);
+    this._handleSubmitForm = handleSubmitForm;
+  }
+
+  _getInputValues() {
+    this._inputList = this._popup.querySelectorAll('.popup__input');
+
+    this._formValues = {};
+    this._inputList.forEach(input => this._formValues[input.name] = input.value);
+
+    return this._formValues;
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+
+    this._popup.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+
+      this._popup.reset();
+    })
+  }
+}
