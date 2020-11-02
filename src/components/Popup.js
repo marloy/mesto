7488e0971453c01977946a8b60e55df4f6cb8aca
marloy <evgeniy.mirea@yandex.ru>
2020-11-02 18:10:41 +0300

    feat: подготовить класс Popup

---
+++ src/components/Popup.js
@@ -0,0 +1,21 @@
+class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+  }
+
+  _handleEscClose() {
+
+  }
+
+  setEventListeners() {
+
+  }
+}
